4f53bad2de86837c7002a5d0096366cc17170538
plastic041 <plastic041@gmail.com>
2023-07-25 15:58:32 +0900

    change: change search page path media -> search

--- src/app/search/@mediaList/page.tsx
+++ src/app/search/@mediaList/page.tsx
@@ -2,11 +2,12 @@ import { Effect, pipe } from 'effect'
 import request, { gql } from 'graphql-request'
 import { z } from 'zod'
 import { P, match } from 'ts-pattern'
+import Link from 'next/link'
 
 const query = gql`
   query ($search: String) {
     Page {
-      media(search: $search) {
+      media(search: $search, type: ANIME, sort: POPULARITY_DESC) {
         id
         title {
           romaji
@@ -68,9 +69,13 @@ export default async function Page({
 
   return match([hasQuery, data])
     .with([true, P.not(P.nullish)], ([_, data]) => (
-      <ul>
+      <ul className="flex flex-col divide-y">
         {data.Page.media.map(media => (
-          <li key={media.id}>{media.title.romaji}</li>
+          <li className="flex flex-col" key={media.id}>
+            <Link className="flex p-4 bg-white hover:bg-slate-100" href={`/media/${media.id}`}>
+              {media.title.romaji}
+            </Link>
+          </li>
         ))}
       </ul>
     ))
